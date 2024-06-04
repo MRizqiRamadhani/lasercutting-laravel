87b6b8c544ac91aaa3242e1a9d7c02383cb08f9a
MRizqiRamadhani <m.rizqiramadhani02@gmail.com>
2024-06-04 10:28:18 +0800

    Update footer.blade.php

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,70 +1,48 @@
-<html lang="en" data-bs-theme="auto">
+<div class="container">
+    <footer class="row row-cols-1 row-cols-sm-2 row-cols-md-5 py-5 my-5">
+        <div class="col mb-3">
+            <img src="/img/Logo.png" alt="" width="75px">
+            <p class="text-body-secondary mt-3">&copy; Laser Cutting Poliban</p>
+        </div>
 
-<head>
-    <script src="/docs/5.3/assets/js/color-modes.js"></script>
+        <div class="col mb-3">
 
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+        </div>
 
-    <title>Footers | Laser Cutting Poliban</title>
+        <div class="col mb-3">
+            <h5>Section</h5>
+            <ul class="nav flex-column">
+                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Home</a></li>
+                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Features</a>
+                </li>
+                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Pricing</a>
+                </li>
+                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">FAQs</a></li>
+                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">About</a></li>
+            </ul>
+        </div>
 
-    <link rel="canonical" href="https://getbootstrap.com/docs/5.3/examples/footers/">
+        <div class="col mb-3">
+            <h5>Section</h5>
+            <ul class="nav flex-column">
+                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Home</a></li>
+                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Features</a>
+                </li>
+                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Pricing</a>
+                </li>
+                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">FAQs</a></li>
+                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">About</a></li>
+            </ul>
+        </div>
 
-    <a href="{{ asset('css/footer.css') }}"></a>
-</head>
-
-<body>
-    <div class="container">
-        <footer class="row row-cols-1 row-cols-sm-2 row-cols-md-5 py-5 my-5">
-            <div class="col mb-3">
-                <img src="/img/Logo.png" alt="" width="75px">
-                <p class="text-body-secondary mt-3">&copy; Laser Cutting Poliban</p>
-            </div>
-
-            <div class="col mb-3">
-
-            </div>
-
-            <div class="col mb-3">
-                <h5>Section</h5>
-                <ul class="nav flex-column">
-                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Home</a></li>
-                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Features</a>
-                    </li>
-                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Pricing</a>
-                    </li>
-                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">FAQs</a></li>
-                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">About</a></li>
-                </ul>
-            </div>
-
-            <div class="col mb-3">
-                <h5>Section</h5>
-                <ul class="nav flex-column">
-                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Home</a></li>
-                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Features</a>
-                    </li>
-                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Pricing</a>
-                    </li>
-                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">FAQs</a></li>
-                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">About</a></li>
-                </ul>
-            </div>
-
-            <div class="col mb-3">
-                <h5>Get In Touch</h5>
-                <ul class="list-unstyled d-flex">
-                    <li class=""><a class="link-body-emphasis" href="#"><i
-                                class="bi bi-instagram h3"></i></a>
-                    </li>
-                    <li class="ms-3"><a class="link-body-emphasis" href="#"><i
-                                class="bi bi-facebook h3"></i></a>
-                    </li>
-                </ul>
-            </div>
-        </footer>
-    </div>
-    <script src="/docs/5.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
-    </script>
-</body>
+        <div class="col mb-3">
+            <h5>Get In Touch</h5>
+            <ul class="list-unstyled d-flex">
+                <li class=""><a class="link-body-emphasis" href="#"><i class="bi bi-instagram h3"></i></a>
+                </li>
+                <li class="ms-3"><a class="link-body-emphasis" href="#"><i class="bi bi-facebook h3"></i></a>
+                </li>
+            </ul>
+        </div>
+    </footer>
+</div>
