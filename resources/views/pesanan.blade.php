7dc3d41a731c627de7a6b6d36ccf2d4ef28c7c5a
MRizqiRamadhani <m.rizqiramadhani02@gmail.com>
2024-06-06 05:15:13 +0800

    Create pesanan.blade.php

---
+++ resources/views/pesanan.blade.php
@@ -0,0 +1,32 @@
+@extends('layouts.navbar')
+@extends('layouts.footer')
+@section('login-logout', 'login')
+@section('pesanan-classes', 'active')
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <script src="/docs/5.3/assets/js/color-modes.js"></script>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Laser Cutting Poliban</title>
+
+    <link rel="canonical" href="https://getbootstrap.com/docs/5.3/examples/carousel/">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
+
+    <!-- Custom styles for this template -->
+    <link href="{{ asset('css/beranda.css') }}" rel="stylesheet">
+</head>
+
+<body>
+
+</body>
+
+</html>
